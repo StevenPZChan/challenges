6406104e0c543f1b1da74ad22cabab618cd76ccf
StevenPZChan <StevenPZChan@gmail.com>
2018-08-03 12:48:17 +0800

    PCC03 StevenPZChan

--- 03/StevenPZChan/tags.py
+++ 03/StevenPZChan/tags.py
@@ -1,11 +1,11 @@
+import re
 from collections import Counter
 from difflib import SequenceMatcher
 from itertools import product
-import re
 
 IDENTICAL = 1.0
 TOP_NUMBER = 10
-RSS_FEED = 'rss.xml'
+RSS_FEED = '../rss.xml'
 SIMILAR = 0.87
 TAG_HTML = re.compile(r'<category>([^<]+)</category>')
 
@@ -14,13 +14,18 @@ def get_tags():
     """Find all tags (TAG_HTML) in RSS_FEED.
     Replace dash with whitespace.
     Hint: use TAG_HTML.findall"""
-    pass
+    with open(RSS_FEED, 'r') as f:
+        return TAG_HTML.findall(f.read().replace('-', ' ').lower(), re.S)
 
 
 def get_top_tags(tags):
     """Get the TOP_NUMBER of most common tags
     Hint: use most_common method of Counter (already imported)"""
-    pass
+    c = Counter()
+    for tag in tags:
+        c[tag] += 1
+    sorted_tags = sorted(c.items(), key=lambda t: t[1], reverse=True)
+    return sorted_tags[:TOP_NUMBER]
 
 
 def get_similarities(tags):
@@ -28,7 +33,10 @@ def get_similarities(tags):
     Hint 1: compare each tag, use for in for, or product from itertools (already imported)
     Hint 2: use SequenceMatcher (imported) to calculate the similarity ratio
     Bonus: for performance gain compare the first char of each tag in pair and continue if not the same"""
-    pass
+
+    for t1, t2 in product(tags, repeat=2):
+        if len(t1) < len(t2) and SequenceMatcher(None, t1, t2).ratio() > SIMILAR:
+            yield (t1, t2)
 
 
 if __name__ == "__main__":
